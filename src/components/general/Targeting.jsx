eb055628119b1460927fed0139c604f9cff65cff
SevenTwentyThree <admin723@SevenTwentyThrees-MacBook-Pro.local>
2026-03-17 10:42:31 -0500

    uppdate&remodel:

--- src/components/general/Targeting.jsx
+++ src/components/general/Targeting.jsx
@@ -4,7 +4,7 @@ function Targeting() {
   return (
     <>
     <h2>Why Invest? Why Us?</h2>
-    <img src="https://obsidi.com/wp-content/uploads/2025/04/black-tech-conferences-scaled.jpg" alt="" className="investPic" />
+    <img src="https://research.com/resized/uploads/fa9d0cf6f74377bbfa2c99fd8ef291f2_0df06883de.webp?format=large" alt="" className="investPic" />
     <p>	<br />•	Founder-led technical expertise
 	<br />•	Lean startup-friendly pricing model
 	<br />•	High-margin service structure
