ad403b501dc58ceda900952aac0ecc56ca851784
SevenTwentyThree <admin723@SevenTwentyThrees-MacBook-Pro.local>
2026-03-25 12:26:44 -0500

    uppdate&remodel:

--- src/components/general/Nav.jsx
+++ src/components/general/Nav.jsx
@@ -32,58 +32,59 @@ function Nav() {
         </div>
         <div className="majorRight">
           <nav className="navMain">
-            <Link to="/home">
               <div className="navBtn">
+            <Link to="/home">
                 <img
                   src="https://cdn-icons-png.flaticon.com/256/25/25694.png"
                   alt=""
                   className="navImg heartbeat"
-                />
+                  />
+                  </Link>
                 <div className="navTitle">Home</div>
               </div>
-            </Link>
 
-            <Link to="/build">
               <div className="navBtn">
+            <Link to="/build">
                 <img
                   src="https://cdn-icons-png.flaticon.com/512/603/603781.png"
                   alt=""
                   className="navImg heartbeat"
                 />
+            </Link>
                 <div className="navTitle">Build</div>
               </div>
-            </Link>
-            <Link to="/home">
               <div className="navBtn">
+            <Link to="/home">
                 <img
                   src="/images/jclLogoMajor.png"
                   alt=""
                   className="gifMe"
+                  
                   onClick={toggleMusic}
-                />
+                  />
+                  </Link>
               </div>
-            </Link>
-            <Link to="/teach">
               <div className="navBtn">
+            <Link to="/teach">
                 <img
                   src="https://www.freeiconspng.com/thumbs/brain-icon-png/brain-2.png"
                   alt=""
                   className="navImg heartbeat"
                 />
+            </Link>
                 <div className="navTitle">Learn</div>
               </div>
-            </Link>
 
-            <Link to="/about">
               <div className="navBtn">
+            <Link to="/about">
                 <img
                   src="https://cdn-icons-png.flaticon.com/512/65/65000.png"
                   alt=""
                   className="navImg heartbeat"
                 />
+            </Link>
                 <div className="navTitle">Vision</div>
               </div>
-            </Link>
           </nav>
         </div>
       </div>
